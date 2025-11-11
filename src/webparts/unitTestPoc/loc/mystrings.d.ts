61fc2d28d0994c53ca70d548ec6cfe0aeaf1c4b9
doug0102 <shea.wow@hotmail.com>
2025-11-11 08:58:48 -0500

    webpart init

---
+++ src/webparts/unitTestPoc/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IUnitTestPocWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'UnitTestPocWebPartStrings' {
+  const strings: IUnitTestPocWebPartStrings;
+  export = strings;
+}
